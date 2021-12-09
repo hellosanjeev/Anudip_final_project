58446bcab4ce856794778e7f05b5c434e3dec48c
hellosanjeev <86008538+hellosanjeev@users.noreply.github.com>
2021-12-09 18:57:53 +0530

    try catch add in program

--- src/main/java/com/example/onlinebakeryshop/OnlinebakeryshopApplication.java
+++ src/main/java/com/example/onlinebakeryshop/OnlinebakeryshopApplication.java
@@ -5,9 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlinebakeryshopApplication {
+	
 
 	public static void main(String[] args) {
+		try{
 		SpringApplication.run(OnlinebakeryshopApplication.class, args);
+		}
+		catch(Exception e){
+		  System.out.println("Port 8080 is in use...");
+		  System.out.println(e);
+		}
+		finally{
+		 System.out.println("If you encounter any problem is occur in application contact to admin or developer ");
+		}
+		
 	}
 
 }
